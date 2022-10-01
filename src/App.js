c0da0cc85d97cbfaa206b2be1648a8e0357e9895
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-09-30 20:03:21 -0500

    Mobile only and vibration added

--- src/App.js
+++ src/App.js
@@ -6,21 +6,33 @@ const App = () => {
 
   const onClick = () => {
     setCount(count + 1)
+    navigator.vibrate(200);
   }
 
   return (
 
-    <div className="h-screen w-screen">
+    <div>
 
-      <div className="h-[20%] bg-[#06283D] flex justify-center items-center">
+      <div className="hidden lg:flex flex-col gap-5 h-screen w-screen justify-center items-center">
+        <h1 className="text-4xl text-[#06283D]">Sorry, this app is designed for mobile/tab only.</h1>
+        <p className="text-xl text-[#06283D]">[Try resizing your browser to test! ;)]</p>
+      </div>
 
-        <h1 className="text-4xl text-[#DFF6FF] ">Slogan Counter</h1>
+      <div className="flex flex-col lg:hidden h-screen w-screen">
+
+        <div className="h-[20%] bg-[#06283D] flex justify-center items-center">
+
+          <h1 className="text-4xl text-[#DFF6FF] ">Slogan Counter</h1>
+
+        </div>
+
+        <button className="h-[80%] w-full bg-[#256D85]" onClick={() => onClick()}><h1 className="text-[20rem] text-[#DFF6FF]">{count}</h1></button>
 
       </div>
 
-      <button className="h-[80%] w-full bg-[#256D85]" onClick={() => onClick()}><h1 className="text-[20rem] text-[#DFF6FF]">{count}</h1></button>
-
     </div>
+
+
   );
 }
 
